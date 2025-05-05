c420bd8b76e4335142d107642f40a21456343bf8
Jacob Buhler <157544127+jacobBuhler@users.noreply.github.com>
2025-05-05 10:22:39 -0700

    quiz 3

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,34 @@ TEST(GuesserTest, smoke_test)
   Guesser object("Secret");
   ASSERT_EQ( 1+1, 2 );
 }
+TEST(GuesserTest, empty_guess)
+{
+  Guesser object("Secret");
+  ASSERT_EQ( 0, 0);
+}
+TEST(GuesserTest, wrong_guess)
+{
+  Guesser object("Secret");
+  object.match("Secrey");
+  ASSERT_EQ( object.remaining(), 2);
+}
+TEST(GuesserTest, bruteforce_guess)
+{
+  Guesser object("Secret");
+  object.match("ssssssss");
+  ASSERT_FALSE(object.match("Secret"));
+}
+TEST(GuesserTest, long_guess)
+{
+  Guesser object("Secret");
+  object.match("areallyreallylongpasswordthatiswayyyyyyylong");
+  ASSERT_FALSE(object.match("Secret"));
+}
+TEST(GuesserTest, reset_guesses)
+{
+  Guesser object("Secret");
+  object.match("Secreq");
+  object.match("Secrew");
+  object.match("Secret");
+  ASSERT_EQ(object.remaining(), 3);
+}
